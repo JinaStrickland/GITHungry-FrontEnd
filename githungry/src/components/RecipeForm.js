e2566cd1a02e54d5477653f73b5a96b2ddbf266d
aysikh <amy.sikhammountry@gmail.com>
2020-11-09 17:26:31 -0500

    got form to work the way I wanted it too

--- githungry/src/components/RecipeForm.js
+++ githungry/src/components/RecipeForm.js
@@ -1,13 +1,13 @@
 import React from 'react'
-import { Formik, Form, Field, ErrorMessage } from 'formik'
+import { Formik, Form, Field, ErrorMessage, FieldArray } from 'formik'
 
 const foodTags = ["Soups and Stews", "Poultry", "Fish", "Healthy", "Dairy Free", "Gluten Free", "Vegan", "Vegetarian", "Egg Free", "Seafood"]
 
 const cuisineTags = ["Appetizer", ""]
 const initialValues = {
   title: "", 
-  ingredients: [], 
-  instructions: [], 
+  ingredients: [""], 
+  instructions: [""], 
   cuisine_type: "",
   meal_type: "", 
   cooking_time: 0, 
@@ -57,7 +57,7 @@ function RecipeForm() {
     initialValues={initialValues}
     onSubmit={onSubmit}
     validate={validate}>
-        <Form class="ui form">
+        <Form className="ui form">
           <div className='form-control'>
           <label>Title: </label>
           <Field
@@ -72,106 +72,141 @@ function RecipeForm() {
           </div>
           <br/>
           <div className='form-control'>
-          <label>Ingredients: </label>
-          <Field
-            as="textarea"
-            id="ingredients"
-            name="ingredients"
-            placeholder="Ingredients..."
-            />
-          <ErrorMessage name="ingredients">
-            {errorMsg => <div className="error">{errorMsg}</div>}
-          </ErrorMessage>
+            <label>Ingredients: </label>
+            <FieldArray name="ingredients">
+              {fieldArrayProps => {
+                console.log('fieldArrayProps', fieldArrayProps)
+                const {push, remove, form} = fieldArrayProps
+                const {values} = form
+                const {ingredients} = values
+                return (
+                <div>
+                  {ingredients.map((ingredient, index) => (
+                    <div key={index}>
+                      <Field name={`ingredients[${index}]`} />
+                      {index > 0 && (
+                        <button type='button' onClick={() => remove(index)}>
+                          {' '}
+                          x{' '}
+                        </button>
+                      )}
+                      <button type='button' onClick={() => push('')}>
+                        {' '}
+                        +{' '}
+                      </button>
+                    </div>
+                ))}
+                </div>
+                )
+            }}
+          </FieldArray>
           </div>
           <br/>
           <div className='form-control'>
           <label>Instructions: </label>
-          <Field
-            as="textarea"
-            id="instructions"
-            name="instructions"
-            placeholder="Instructions..."
-            />
-          <ErrorMessage name="instructions">
-            {errorMsg => <div className="error">{errorMsg}</div>}
-          </ErrorMessage>
+          <FieldArray name="instructions">
+            {fieldArrayProps => {
+              const {push, remove, form} = fieldArrayProps
+              const {values} = form
+              const {instructions} = values
+              return(
+                <div>
+                  {instructions.map((instruction, index) => (
+                    <div key={index}>
+                      <Field name={`instructions[${index}]`}/>
+                      {index > 0 && (
+                        <button type='button' onClick={() => remove(index)}>
+                          {' '}
+                          x{' '}
+                        </button>
+                      )}
+                      <button type='button' onClick={() => push(' ')}>
+                        {' '}
+                        +{' '}
+                      </button>
+                    </div>
+                  ))}
+                </div>
+              )
+            }}
+          </FieldArray>
+
           </div>
           <br/>
           <div className='form-control'>
           <label>Type Of Cuisine: </label>
-          <Field
-            type="text"
-            id="cuisine_type"
-            name="cuisine_type"
-            placeholder="Cuisine Type"
-            />
-          <ErrorMessage name="cuisine_type">
-            {errorMsg => <div className="error">{errorMsg}</div>}
-          </ErrorMessage>
+            <Field
+              type="text"
+              id="cuisine_type"
+              name="cuisine_type"
+              placeholder="Cuisine Type"
+              />
+            <ErrorMessage name="cuisine_type">
+              {errorMsg => <div className="error">{errorMsg}</div>}
+            </ErrorMessage>
           </div>
           <br/>
           <div className='form-control'>
           <label>Type of Meal: </label>
-          <Field
-            type="text"
-            id="meal_type"
-            name="meal_type"
-            />
-          <ErrorMessage name="meal_type">
-            {errorMsg => <div className="error">{errorMsg}</div>}
-          </ErrorMessage>
+            <Field
+              type="text"
+              id="meal_type"
+              name="meal_type"
+              />
+            <ErrorMessage name="meal_type">
+              {errorMsg => <div className="error">{errorMsg}</div>}
+            </ErrorMessage>
           </div>
           <br/>
           <div className='form-control'>
           <label>Total Cooking Time: </label>
-          <Field
-            type="number"
-            id="cooking_time"
-            name="cooking_time"
-            />
+            <Field
+              type="number"
+              id="cooking_time"
+              name="cooking_time"
+              />
           </div>
           <br/>
           <div className='form-control'>
           <label>Servings: </label>
-          <Field
-            type="number"
-            id="servings"
-            name="servings"
-            />
+            <Field
+                type="number"
+                id="servings"
+                name="servings"
+              />
           </div>
           <br/>
           <div className='form-control'>
           <label>Select Tags: </label>
-          {foodTags.map(foodTag => {
-          return (
+            {foodTags.map(foodTag => {
+            return (
             <div>
-          <div> {foodTag} </div>
-          <Field 
-             
-            class="ui checkbox"
-            type="checkbox"
-            id={foodTag}
-            name={foodTag}
-            /> 
+              <div> {foodTag} </div>
+              <Field 
+                  class="ui checkbox"
+                  type="checkbox"
+                  id={foodTag}
+                  name={foodTag}
+                /> 
             </div>
-          )
-          })
-        }
-          <ErrorMessage name="tags">
-            {errorMsg => <div className="error">{errorMsg}</div>}
-          </ErrorMessage>
+              )
+            })
+          }
+            <ErrorMessage name="tags">
+              {errorMsg => <div className="error">{errorMsg}</div>}
+            </ErrorMessage>
           </div>
           <br/>
           <div className='form-control'>
           <label>Image URL: </label>
-          <Field
-            type="text"
-            id="image"
-            name="image"
-            />
-          <ErrorMessage name="image">
-            {errorMsg => <div className="error">{errorMsg}</div>}
-          </ErrorMessage>
+            <Field
+                type="text"
+                id="image"
+                name="image"
+              />
+            <ErrorMessage name="image">
+              {errorMsg => <div className="error">{errorMsg}</div>}
+            </ErrorMessage>
           </div>
           <br/>
           <button>Submit</button>
